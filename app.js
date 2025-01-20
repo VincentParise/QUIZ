13403fb516a2019fe8c9d623d91c2c33d3214a0a
vincentp73000 <vincentp73000@gmail.com>
2025-01-20 14:03:03 +0100

    Fichier css

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
 const responses = ["c", "a", "b", "a", "c"];
 const emojis = ["✔️", "✨", "👀", "😭", "👎"];
+
+// Objet questions / réponses
+const dataQuizz = {
+    questionUn : {
+        libelle :"Qui est sacré empereur de France le 2vdécemb re 1804",
+        choix : ["clovis", "Abraham Lincoln", "Napoleon Bonaparte"],
+        reponse : 1
+    },
+    questionDeux : {
+        libelle :"Quand la déclaration d'indépendance des USA a t'elle été voté",
+        choix : ["4 Juillet 2018", "18 avril 2012", "5 mai 1789"],
+        reponse : 2
+    },
+}
+
+// Itération sur un objet
+for (let numQuestion in dataQuizz.questionUn){
+
+    console.log(numQuestion)
+    // for (let element in numQuestion.questionUn){
+    //     console.log(element);
+    // }
+
+}
